5a0d38603f716af7addc8e88480cfdfa83430336
Dragan <d.novakovic1810@yahoo.com>
2018-09-08 13:17:43 +0200

    moved

--- fishineerProject server side/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/lol', (req, res) => res.send('<b>LOL</b>'));
-
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
