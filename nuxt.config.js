d36b854217eb20e3315ac791f733338acfac65a4
Dragan <d.novakovic1810@yahoo.com>
2018-09-24 16:11:15 +0200

    update to nuxt.js 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,75 @@
+const pkg = require('./package')
 
-const serveStatic = require('serve-static')
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
-
-
-  serverMiddleware: [
-    // Will register redirect-ssl npm package
-    'redirect-ssl',
-
-    // Will register file from project api directory to handle /api/* requires
-    { path: '/api', handler: '~/myRedirectPage.js' }
-  ],
+  mode: 'universal',
 
   /*
   ** Headers of the page
   */
   head: {
-    title: 'hello-nuxt',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Fishoneer' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
+
   /*
-  ** Customize the progress bar color
+  ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#FFFFFF' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'vuetify/src/stylus/main.styl'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/vuetify'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
   /*
   ** Build configuration
   */
   build: {
     /*
-    ** Run ESLint on save
+    ** You can extend webpack config here
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+    extend(config, ctx) {
+      
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
       }
     }
   }
 }
-
